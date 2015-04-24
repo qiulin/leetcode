a56b8ca4ba8a020e621e35c930c88aea7e19acff
qiulin <xcodeman@gmail.com>
2015-04-24 18:46:05 +0800

    initial code structure and test file for problem LRUCache

---
+++ Python/LRUCache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class LRUCache:
+
+    # @param capacity, an integer
+    def __init__(self, capacity):
+        pass
+
+    # @return an integer
+    def get(self, key):
+        pass
+
+    # @param key, an integer
+    # @param value, an integer
+    # @return nothing
+    def set(self, key, value):
+        pass
